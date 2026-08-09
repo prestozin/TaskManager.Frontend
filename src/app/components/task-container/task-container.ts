29baffb6ceda9bc648d2762d307d9e55d1667616
Mateus Bacelar <mateusbacelar@outlook.com>
2026-08-09 03:21:52 -0300

    ajuste de component de tarefa

--- src/app/components/task-container/task-container.ts
+++ src/app/components/task-container/task-container.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { TaskComponent } from "../task/task";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-container',
   imports: [
-    TaskComponent
+    TaskComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './task-container.html',
   styleUrl: './task-container.scss',
 })
 export class TaskContainerComponent {
   
+  allTasksForm = new FormGroup({
+    title: new FormControl('',{ nonNullable: true }),
+    completed: new FormControl(false, { nonNullable: true })
+  }); 
 }
