006ba8a61fa685b64002138016a411fcd021e0db
Mateus Bacelar <mateusbacelar@outlook.com>
2026-08-10 01:45:06 -0300

    criação de header do container e filtros

--- src/app/components/task-container/task-container.ts
+++ src/app/components/task-container/task-container.ts
@@ -14,7 +14,10 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class TaskContainerComponent {
   
   allTasksForm = new FormGroup({
-    title: new FormControl('',{ nonNullable: true }),
+    title: new FormControl('', { nonNullable: true }),
     completed: new FormControl(false, { nonNullable: true })
   }); 
+
+  searchControl = new FormControl('', {nonNullable: true});
+
 }
