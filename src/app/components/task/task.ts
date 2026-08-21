964f87f42a966341ffc81f9247452c104ffd58ba
Mateus Bacelar <mateusbacelar@outlook.com>
2026-08-21 16:43:49 -0300

    formatação de data, e mudança de cores para status e prioridade

--- src/app/components/task/task.ts
+++ src/app/components/task/task.ts
@@ -1,11 +1,14 @@
 import { Component, input } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TaskResponse } from '../../types/task/task-response';
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    DatePipe,
+    NgClass
   ],
   templateUrl: './task.html',
   styleUrl: './task.scss',
@@ -15,7 +18,15 @@ export class TaskComponent {
   task = input.required<TaskResponse>();
 
   taskForm = new FormGroup({
-    title: new FormControl('',{ nonNullable: true }),
+    title: new FormControl('', { nonNullable: true }),
     completed: new FormControl(false, { nonNullable: true })
   });
+
+  normalizeClass(value: string): string {
+    return value
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/\s+/g, '-');
+  }
 }
